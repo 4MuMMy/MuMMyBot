1648e3ec5d02da9ae1bdd9343f07327f266d8507
MuMMy <43711943+4MuMMy@users.noreply.github.com>
2021-12-31 03:21:58 +0300

    Create cDirectInput.cs

---
+++ Source Code/cDirectInput.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace MuMMyBot
+{
+    class cDirectInput
+    {
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        public enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            ExtendedKey = 0x0001,
+            KeyUp = 0x0002,
+            Unicode = 0x0004,
+            Scancode = 0x0008,
+        }
+
+
+        public static void Send_Key(short Keycode, int KeyUporDown)
+        {
+            INPUT[] InputData = new INPUT[1];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = Keycode;
+            InputData[0].ki.dwFlags = KeyUporDown;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(1, InputData, Marshal.SizeOf(InputData[0]));
+        }
+
+    }
+}
